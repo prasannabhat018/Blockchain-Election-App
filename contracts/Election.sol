bb9e2027ec4d45cbb7610c5cce02f4ca8db8ca05
prasannabhat018 <prasannabhat018@gmail.com>
2020-07-13 09:50:37 +0530

    Has voting features without any time related and registration constraints

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,43 @@ pragma solidity >0.5.2;
 
 contract Election {
 
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+
+    event votedEvent(
+        uint indexed_candidateId
+    );
+
 
     constructor () public{
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        
+        voters[msg.sender] = true;
+
+        candidates[_candidateId].voteCount++;
+
+        emit votedEvent(_candidateId);
     }
 
 }
